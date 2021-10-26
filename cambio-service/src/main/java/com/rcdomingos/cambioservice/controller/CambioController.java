6e8ef13d422fd9d84978c9f9a218ca9bc7e53af7
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2021-10-25 21:39:20 -0300

    Added Spring Jpa, MySQL and Flyway to the microservice

--- cambio-service/src/main/java/com/rcdomingos/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/com/rcdomingos/cambioservice/controller/CambioController.java
@@ -1,6 +1,8 @@
 package com.rcdomingos.cambioservice.controller;
 
 import com.rcdomingos.cambioservice.model.Cambio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,15 @@ import java.math.BigDecimal;
 @RequestMapping("cambio-service")
 public class CambioController {
 
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/{amount}/{from}/{to}")
     public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
                             @PathVariable("from") String from,
                             @PathVariable("to") String to) {
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        String port = environment.getProperty("local.server.port");
+
+        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
     }
 }
