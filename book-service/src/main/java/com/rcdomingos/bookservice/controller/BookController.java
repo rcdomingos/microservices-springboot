e9841adebf66dabf1a394f4ecd62e13fe53e81ad
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2021-10-27 20:57:16 -0300

    added communication between book-service and cambio-service

--- book-service/src/main/java/com/rcdomingos/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/rcdomingos/bookservice/controller/BookController.java
@@ -2,12 +2,16 @@ package com.rcdomingos.bookservice.controller;
 
 import com.rcdomingos.bookservice.model.Book;
 import com.rcdomingos.bookservice.repository.BookRepository;
+import com.rcdomingos.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -26,8 +30,19 @@ public class BookController {
         var book = repository.findById(id);
 
         if (book.isEmpty()) throw new IllegalArgumentException("Book Not Found");
+
         String port = environment.getProperty("local.server.port");
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.get().getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+        var response = new RestTemplate().getForEntity("http://localhost:8000/cambio-service/{amount}/{from}/{to}", Cambio.class, params);
+        var cambio = response.getBody();
+
         book.get().setEnvironment(port);
+        book.get().setPrice(cambio.getConvertedValue());
+
         return book.get();
     }
 }
