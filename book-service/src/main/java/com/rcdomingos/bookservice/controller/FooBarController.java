7ac146b689f3656cb780603bfeae08f63e4a1ae4
Reginaldo C Domingos <39439355+rcdomingos@users.noreply.github.com>
2021-11-02 11:19:30 -0300

    added circuit breaker, rate limiter and bulkhead com Resilience4j

--- book-service/src/main/java/com/rcdomingos/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/rcdomingos/bookservice/controller/FooBarController.java
@@ -1,5 +1,8 @@
 package com.rcdomingos.bookservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +19,8 @@ public class FooBarController {
 
     private final Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
+    // Retry -> usado para configurar tentativas de conexão quando gera falha
+    // fallback -> metodo que sera chamado apos as tentativas e continuar a falha
     @GetMapping("/foo-bar")
     @Retry(name = "foo-bar", fallbackMethod = "myFallbackMethod")
     public String fooBar() {
@@ -26,6 +31,37 @@ public class FooBarController {
         //return "Foo-Bar!!";
         return response.getBody();
     }
+
+    // CircuitBreaker -> gerencia as chamadas para evitar excesso de chamadas com erros,
+    // evitando sobrecargas no serviço
+    @GetMapping("/foo-bar/circuit-breaker")
+    @CircuitBreaker(name = "default", fallbackMethod = "myFallbackMethod")
+    public String foobarCircuitBreaker() {
+        String urlInvalida = "http://localhost:8080/foo-bar";
+        logger.info("Request to /foo-bar/circuit-breaker is received!");
+        ResponseEntity<String> response = new RestTemplate()
+                .getForEntity(urlInvalida, String.class);
+        return response.getBody();
+    }
+
+    // RateLimiter -> controla a frequencia de determinado evento podendo configurar limites de requisições
+    @GetMapping("/foo-bar/rate-limiter")
+    @RateLimiter(name = "default")
+    public String foobarRateLimiter() {
+        logger.info("Request to /foo-bar/rate-limiter is received!");
+        return "Foo-Bar!!";
+    }
+
+    // Bulkhead -> definir limites de chamadas concorrentes
+    @GetMapping("/foo-bar/bulkhead")
+    @Bulkhead(name = "default")
+    public String foobarBulkhead() {
+        logger.info("Request to /foo-bar/bulkhead is received!");
+        return "Foo-Bar!!";
+    }
+
+
+
 
     // depois dos retrys sem sucesso, chama o fallbackMethod
     // pode customizar por Exception mais especialista
